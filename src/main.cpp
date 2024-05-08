524923eb4cd4806ed6c550f1e8c1b6b7d55f6944
Austin D'Acquisto <austindacquisto@gmail.com>
2024-05-08 16:06:06 -0700

    Changes made to variable capitalization. More setup code added for various pins and components.

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,25 @@
 
 // ===== SENSORS =====
 // - Button
-const int buttonPin = 0; // TODO: Define pins
+const int BUTTON_PIN = 0; // TODO: Define pins
 // - Encoder A
-const int encoderPinA1 = 0; // TODO: Define pins
-const int encoderPinA2 = 0;
+const int ENCODER_PIN_A1 = 0, ENCODER_PIN_A2 = 0; // TODO: Define pins
 // - Encoder B
-const int encoderPinB1 = 0;
-const int encoderPinB2 = 0;
-// - Line Sensor A
-const int lineSensorPinA = A0; // TODO: Define pins
-// - Line Sensor B
-const int lineSensorPinB = A0;
+const int ENCODER_PIN_B1 = 0, ENCODER_PIN_B2 = 0; // TODO: Define pins
+// - Line Sensor A (front)
+const int LINE_SENSOR_PIN_A1 = 0, LINE_SENSOR_PIN_A2 = 0, LINE_SENSOR_PIN_A3 = 0; // TODO: Define pins
+// - Line Sensor B (side)
+const int LINE_SENSOR_PIN_B = 0; // TODO: Define pins
 // - Ultrasonic Sensor
-const int ultrasonicTrigPin = 0; // TODO: Define pins
-const int ultrasonicEchoPin = 0;
+const int ULTRASONIC_TRIG_PIN = 0, ULTRASONIC_ECHO_PIN = 0; // TODO: Define pins
 
 // ===== OUTPUTS =====
 // - LED
-const int ledPin = LED_BUILTIN;
+const int LED_PIN = LED_BUILTIN;
 // - Servo Motor A
-const int servoPinA1 = 0; // TODO: Define pins
-const int servoPinA2 = 0;
+const int SERVO_PIN_A1 = 0, SERVO_PIN_A2 = 0; // TODO: Define pins
 // - Servo Motor B
-const int servoPinB1 = 0;
-const int servoPinB2 = 0;
+const int SERVO_PIN_B1 = 0, SERVO_PIN_B2 = 0; // TODO: Define pins
 // - Stepper Motor A
 // - Stepper Motor B
 
@@ -38,26 +33,35 @@ DRV8833 motorDriver = DRV8833();
 
 // Main setup
 void setup() {
-  // Initialize pins
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
-  pinMode(ultrasonicTrigPin, OUTPUT);
-  pinMode(ultrasonicEchoPin, INPUT);
+  // Sensors
+  pinMode(BUTTON_PIN, INPUT); // Button
+  pinMode(ENCODER_PIN_A1, INPUT_PULLUP); // Encoder A
+  pinMode(ENCODER_PIN_A2, INPUT_PULLUP);
+  pinMode(ENCODER_PIN_B1, INPUT_PULLUP); // Encoder B
+  pinMode(ENCODER_PIN_B2, INPUT_PULLUP);
+  pinMode(LINE_SENSOR_PIN_A1, INPUT); // Line Sensor A
+  pinMode(LINE_SENSOR_PIN_A2, INPUT);
+  pinMode(LINE_SENSOR_PIN_A3, INPUT);
+  pinMode(LINE_SENSOR_PIN_B, INPUT); // Line Sensor B
+  pinMode(ULTRASONIC_TRIG_PIN, OUTPUT); // Ultrasonic Sensor
+  pinMode(ULTRASONIC_ECHO_PIN, INPUT);
+  // Outputs
+  pinMode(LED_PIN, OUTPUT); // LED
 
   // Initialize Serial port
   Serial.begin(9600);
   while (!Serial); // Waits for the Serial port to connect.
-
+  
   // Attach motors
-  motorDriver.attachMotorA(servoPinA1, servoPinA2);
-  motorDriver.attachMotorB(servoPinB1, servoPinB2);
+  motorDriver.attachMotorA(SERVO_PIN_A1, SERVO_PIN_A2);
+  motorDriver.attachMotorB(SERVO_PIN_B1, SERVO_PIN_B2);
 
 }
 
 // Main loop
 void loop() {
   // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
+  buttonState = digitalRead(BUTTON_PIN);
 
   Serial.println(buttonState);
 
@@ -65,14 +69,14 @@ void loop() {
   // if it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
     // turn LED on:
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(LED_PIN, HIGH);
 
     // Move motors
     motorDriver.motorAForward();
     motorDriver.motorBForward();
   } else {
     // turn LED off:
-    digitalWrite(ledPin, LOW);
+    digitalWrite(LED_PIN, LOW);
 
     // Stop motors
     motorDriver.motorAStop();
