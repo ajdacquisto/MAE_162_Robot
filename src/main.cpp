04b6ab50785729ede42f0716b2a922a733fa4676
Austin D'Acquisto <austindacquisto@gmail.com>
2024-05-08 16:22:59 -0700

    Adds basic code for stepper motor.

--- src/main.cpp
+++ src/main.cpp
@@ -24,13 +24,19 @@ const int SERVO_PIN_A1 = 0, SERVO_PIN_A2 = 0; // TODO: Define pins
 // - Servo Motor B
 const int SERVO_PIN_B1 = 0, SERVO_PIN_B2 = 0; // TODO: Define pins
 // - Stepper Motor A
+const int STEPPER_PIN_A1 = 0, STEPPER_PIN_A2 = 0, STEPPER_PIN_A3 = 0, STEPPER_PIN_A4 = 0; // TODO: Define pins
+const int STEPPER_A_STEPS_PER_REVOLUTION = 0;  // TODO: Set this value
 // - Stepper Motor B
+const int STEPPER_PIN_B1 = 0, STEPPER_PIN_B2 = 0, STEPPER_PIN_B3 = 0, STEPPER_PIN_B4 = 0; // TODO: Define pins
+const int STEPPER_B_STEPS_PER_REVOLUTION = 0;  // TODO: Set this value
 
 // Other Global Variables
 int buttonState = 0;   
 
 // Objects
 DRV8833 motorDriver = DRV8833();
+Stepper stepperMotorA(STEPPER_A_STEPS_PER_REVOLUTION, STEPPER_PIN_A1, STEPPER_PIN_A2, STEPPER_PIN_A3, STEPPER_PIN_A4);
+Stepper stepperMotorB(STEPPER_B_STEPS_PER_REVOLUTION, STEPPER_PIN_B1, STEPPER_PIN_B2, STEPPER_PIN_B3, STEPPER_PIN_B4);
 
 // Main setup
 void setup() {
@@ -53,9 +59,13 @@ void setup() {
   Serial.begin(9600);
   while (!Serial); // Waits for the Serial port to connect.
 
-  // Attach motors
+  // Attach servo motors
   motorDriver.attachMotorA(SERVO_PIN_A1, SERVO_PIN_A2);
   motorDriver.attachMotorB(SERVO_PIN_B1, SERVO_PIN_B2);
+
+  // Set stepper motor speed
+  stepperMotorA.setSpeed(60);  // Set the speed to 60 RPMs
+  stepperMotorB.setSpeed(60);
 
 }
 
@@ -75,6 +85,11 @@ void loop() {
     // Move motors
     motorDriver.motorAForward();
     motorDriver.motorBForward();
+
+    // Rotate stepper motor
+    stepperMotorA.step(STEPPER_A_STEPS_PER_REVOLUTION);
+    delay(500);
+
   } else {
     // turn LED off:
     digitalWrite(LED_PIN, LOW);
