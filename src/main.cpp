002a6907504919b3443f8150d9db4b9d92904973
Austin D'Acquisto <austindacquisto@gmail.com>
2024-05-08 13:51:26 -0700

    This commit adds framework for IO pins at the top of the main.cpp file.  It also adds a DRV8833 library for controlling the motor driver.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,60 @@
 #include <Arduino.h>
+#include "DRV8833.h"
 
-const int buttonPin = 2;
+// ===== SENSORS =====
+// - Button
+const int buttonPin = 0; // TODO: Define pins
+// - Encoder A
+const int encoderPinA1 = 0; // TODO: Define pins
+const int encoderPinA2 = 0;
+// - Encoder B
+const int encoderPinB1 = 0;
+const int encoderPinB2 = 0;
+// - Line Sensor A
+const int lineSensorPinA = A0; // TODO: Define pins
+// - Line Sensor B
+const int lineSensorPinB = A0;
+// - Ultrasonic Sensor
+const int ultrasonicTrigPin = 0; // TODO: Define pins
+const int ultrasonicEchoPin = 0;
 
+// ===== OUTPUTS =====
+// - LED
+const int ledPin = LED_BUILTIN;
+// - Servo Motor A
+const int servoPinA1 = 0; // TODO: Define pins
+const int servoPinA2 = 0;
+// - Servo Motor B
+const int servoPinB1 = 0;
+const int servoPinB2 = 0;
+// - Stepper Motor A
+// - Stepper Motor B
+
+// Other Global Variables
 int buttonState = 0;   
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(buttonPin, INPUT);
+// Objects
+DRV8833 motorDriver = DRV8833();
 
+// Main setup
+void setup() {
+  // Initialize pins
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(ultrasonicTrigPin, OUTPUT);
+  pinMode(ultrasonicEchoPin, INPUT);
+
+  // Initialize Serial port
   Serial.begin(9600);
+  while (!Serial); // Waits for the Serial port to connect.
+
+  // Attach motors
+  motorDriver.attachMotorA(servoPinA1, servoPinA2);
+  motorDriver.attachMotorB(servoPinB1, servoPinB2);
+
 }
 
+// Main loop
 void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
@@ -21,9 +65,17 @@ void loop() {
   // if it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
     // turn LED on:
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(ledPin, HIGH);
+
+    // Move motors
+    motorDriver.motorAForward();
+    motorDriver.motorBForward();
   } else {
     // turn LED off:
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(ledPin, LOW);
+
+    // Stop motors
+    motorDriver.motorAStop();
+    motorDriver.motorBStop();
   }
 }
