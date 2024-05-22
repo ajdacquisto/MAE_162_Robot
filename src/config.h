792277fdc1a8db3ce491c170cc2e4a0056f2fa32
Austin D'Acquisto <austindacquisto@gmail.com>
2024-05-22 12:40:03 -0700

    Setup for servo B and encoder B.

--- src/config.h
+++ src/config.h
@@ -6,9 +6,9 @@
 // - Button
 const int BUTTON_PIN = 7; // TODO: Define pins
 // - Encoder A
-const int ENCODER_PIN_A1 = 2, ENCODER_PIN_A2 = 3; // TODO: Define pins
+const int ENCODER_PIN_A1 = 18, ENCODER_PIN_A2 = 19; // TODO: Define pins
 // - Encoder B
-const int ENCODER_PIN_B1 = 0, ENCODER_PIN_B2 = 0; // TODO: Define pins
+const int ENCODER_PIN_B1 = 20, ENCODER_PIN_B2 = 21; // TODO: Define pins
 // - Line Sensor A (front)
 const int LINE_SENSOR_PIN_A1 = 0, LINE_SENSOR_PIN_A2 = 0, LINE_SENSOR_PIN_A3 = 0; // TODO: Define pins
 // - Line Sensor B (side)
@@ -22,7 +22,7 @@ const int LED_PIN = LED_BUILTIN;
 // - Servo Motor A
 const int SERVO_PIN_A1 = 11, SERVO_PIN_A2 = 12; // TODO: Define pins
 // - Servo Motor B
-const int SERVO_PIN_B1 = 0, SERVO_PIN_B2 = 0; // TODO: Define pins
+const int SERVO_PIN_B1 = 9, SERVO_PIN_B2 = 10; // TODO: Define pins
 // - Stepper Motor A
 const int STEPPER_PIN_A1 = 0, STEPPER_PIN_A2 = 0, STEPPER_PIN_A3 = 0, STEPPER_PIN_A4 = 0; // TODO: Define pins
 const int STEPPER_A_STEPS_PER_REVOLUTION = 0;  // TODO: Set this value
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include "Encoder.h"
 
 // Other Global Variables
-int buttonState = 0;   
+int buttonState = 0;
 
 // Objects
 DRV8833 motorDriver = DRV8833();
@@ -21,19 +21,32 @@ Encoder encoderB(ENCODER_PIN_B1, ENCODER_PIN_B2);
 
 // ===== STATES =====
 // Define states
-enum State {
+enum State
+{
   TEST,
   IDLE,
   FOLLOW_LINE,
   AVOID_OBSTACLE
 };
 
+enum LEDState
+{
+  OFF = LOW,
+  ON = HIGH
+};
+
+enum ButtonState
+{
+  PRESSED = HIGH,
+  UNPRESSED = LOW
+};
+
 // Current state variable
 State currentState = IDLE;
 
 // ===== TIME CONTROL =====
-unsigned long lastStateChangeTime = 0;  // Tracks the last time the state changed
-unsigned long stateDuration = 0;        // Duration to stay in a particular state, if necessary
+unsigned long lastStateChangeTime = 0; // Tracks the last time the state changed
+unsigned long stateDuration = 0;       // Duration to stay in a particular state, if necessary
 
 // ===== FUNCTION PROTOTYPES =====
 void handleTest();
@@ -43,9 +56,13 @@ void handleAvoidObstacle();
 void initializePins();
 void initializeSerialPort();
 void attachServoMotors();
+void zeroEncoders();
+void turnLED(LEDState state);
+ButtonState getButtonState();
 
 // ===== MAIN SETUP =====
-void setup() {
+void setup()
+{
   Serial.println("Starting...");
 
   initializePins();
@@ -56,85 +73,90 @@ void setup() {
   attachServoMotors();
 
   // Set stepper motor speed
-  stepperMotorA.setSpeed(60);  // Set the speed to 60 RPM
+  stepperMotorA.setSpeed(60); // Set the speed to 60 RPM
   stepperMotorB.setSpeed(60);
 
-  // Initilize the encoder
-  encoderA.write(0);
-  //encoderB.write(0);
-
+  // Zero the encoders
+  zeroEncoders();
 }
 
 // ===== MAIN LOOP =====
-void loop() {
-  switch (currentState) {
-    case TEST:
-      handleTest();
-      break;
-    case IDLE:
-      handleIdle();
-      break;
-    case FOLLOW_LINE:
-      handleFollowLine();
-      break;
-    case AVOID_OBSTACLE:
-      handleAvoidObstacle();
-      break;
+void loop()
+{
+  switch (currentState)
+  {
+  case TEST:
+    handleTest();
+    break;
+  case IDLE:
+    handleIdle();
+    break;
+  case FOLLOW_LINE:
+    handleFollowLine();
+    break;
+  case AVOID_OBSTACLE:
+    handleAvoidObstacle();
+    break;
   }
 }
 
 // ===== STATE HANDLERS =====
-void changeState(State newState, unsigned long duration = 0) {
+void changeState(State newState, unsigned long duration = 0)
+{
   currentState = newState;
   lastStateChangeTime = millis();
-  stateDuration = duration;  // If you want a state to last a minimum amount of time, use this
+  stateDuration = duration; // If you want a state to last a minimum amount of time, use this
 }
 
-void handleTest() {
+void handleTest()
+{
   // Code for testing
 
   // read the state of the pushbutton value:
-  buttonState = !digitalRead(BUTTON_PIN);  // Invert the reading here
+  ButtonState buttonState = getButtonState();
 
-  long encoderValue = encoderA.read();
+  // read the state of the encoder value:
+  long encoderValueA = encoderA.read();
+  long encoderValueB = encoderB.read();
 
   Serial.print("Encoder Value: ");
-  Serial.println(encoderValue);
-  //Serial.println(buttonState);
+  Serial.print(encoderValueA);
+  Serial.print(", ");
+  Serial.println(encoderValueB);
+  // Serial.println(buttonState);
 
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(LED_PIN, HIGH);
+  if (buttonState == PRESSED)
+  {
+    turnLED(ON);
 
     // Move motors
     motorDriver.motorAForward();
-    //motorDriver.motorBForward();
+    motorDriver.motorBForward();
 
     // Rotate stepper motor
-    //stepperMotorA.step(STEPPER_A_STEPS_PER_REVOLUTION);
-    
-
-  } else {
-    // turn LED off:
-    digitalWrite(LED_PIN, LOW);
+    // stepperMotorA.step(STEPPER_A_STEPS_PER_REVOLUTION);
+  }
+  else
+  {
+    turnLED(OFF);
 
     // Stop motors
     motorDriver.motorAStop();
-    //motorDriver.motorBStop();
-    
+    motorDriver.motorBStop();
   }
 }
 
-void handleIdle() {
+void handleIdle()
+{
   /*if (conditionToStartFollowingLine) {
     changeState(FOLLOW_LINE, 5000);  // Stay in FOLLOW_LINE for at least 5000 ms
   }*/
 }
 
-
-void handleFollowLine() {
+void handleFollowLine()
+{
   /*if (millis() - lastStateChangeTime > stateDuration) {  // Ensure at least stateDuration has passed
     if (lineLost) {
       changeState(IDLE);
@@ -144,7 +166,8 @@ void handleFollowLine() {
   }*/
 }
 
-void handleAvoidObstacle() {
+void handleAvoidObstacle()
+{
   /*// Assume we need to avoid obstacles for a minimum of 3 seconds
   if (millis() - lastStateChangeTime > 3000) {  // Stay in AVOID_OBSTACLE for at least 3000 ms
     if (obstacleCleared) {
@@ -154,8 +177,9 @@ void handleAvoidObstacle() {
 }
 
 // ===== HELPER FUNCTIONS =====
-void initializePins() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP); // Button
+void initializePins()
+{
+  pinMode(BUTTON_PIN, INPUT_PULLUP);     // Button
   pinMode(ENCODER_PIN_A1, INPUT_PULLUP); // Encoder A
   pinMode(ENCODER_PIN_A2, INPUT_PULLUP);
   pinMode(ENCODER_PIN_B1, INPUT_PULLUP); // Encoder B
@@ -163,18 +187,37 @@ void initializePins() {
   pinMode(LINE_SENSOR_PIN_A1, INPUT); // Line Sensor A
   pinMode(LINE_SENSOR_PIN_A2, INPUT);
   pinMode(LINE_SENSOR_PIN_A3, INPUT);
-  pinMode(LINE_SENSOR_PIN_B, INPUT); // Line Sensor B
+  pinMode(LINE_SENSOR_PIN_B, INPUT);    // Line Sensor B
   pinMode(ULTRASONIC_TRIG_PIN, OUTPUT); // Ultrasonic Sensor
   pinMode(ULTRASONIC_ECHO_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT); // LED
 }
 
-void initializeSerialPort() {
+void initializeSerialPort()
+{
   Serial.begin(9600); // Initialize Serial port
-  while (!Serial); // Waits for the Serial port to connect.
+  while (!Serial)
+    ; // Waits for the Serial port to connect.
 }
 
-void attachServoMotors() {
+void attachServoMotors()
+{
   motorDriver.attachMotorA(SERVO_PIN_A1, SERVO_PIN_A2);
-  //motorDriver.attachMotorB(SERVO_PIN_B1, SERVO_PIN_B2);
+  motorDriver.attachMotorB(SERVO_PIN_B1, SERVO_PIN_B2);
+}
+
+void zeroEncoders()
+{
+  encoderA.write(0);
+  encoderB.write(0);
+}
+
+void turnLED(LEDState state)
+{
+  digitalWrite(LED_PIN, state);
+}
+
+ButtonState getButtonState()
+{
+  return !digitalRead(BUTTON_PIN) == HIGH ? PRESSED : UNPRESSED;
 }
