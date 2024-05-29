381adff9d99e3aca6a8a8839517dc674ae1e0f53
Austin D'Acquisto <austindacquisto@gmail.com>
2024-05-28 19:34:35 -0700

    Reorganization of config file.

--- src/config.h
+++ src/config.h
@@ -4,60 +4,111 @@
 
 #include <Arduino.h>
 
-// ===== SETTINGS =====
+// =============================
+// ===== LOCATION SETTINGS =====
+// =============================
+
+// - Locations
 enum LOCATION {
-    FIRST_ON_LEFT,   // 0
-    SECOND_ON_LEFT,  // 1
-    THIRD_ON_LEFT,   // 2
-    FIRST_ON_RIGHT,  // 3
-    SECOND_ON_RIGHT, // 4
-    THIRD_ON_RIGHT   // 5
+  FIRST_ON_LEFT,   // 0
+  SECOND_ON_LEFT,  // 1
+  THIRD_ON_LEFT,   // 2
+  FIRST_ON_RIGHT,  // 3
+  SECOND_ON_RIGHT, // 4
+  THIRD_ON_RIGHT   // 5
 };
+
+// - Pickup and dropoff locations
 const int PICKUP_LOCATION_1 = FIRST_ON_LEFT;
 const int PICKUP_LOCATION_2 = SECOND_ON_LEFT;
 const int DROPOFF_LOCATION = SECOND_ON_RIGHT;
-// ===== SENSORS =====
+
+// =================================
+// ===== INPUT PIN ASSIGNMENTS =====
+// =================================
+
 // - Button
 const int BUTTON_PIN = 7;
-// - Encoder A
+
+// - Encoders
 const int ENCODER_PIN_A1 = 18, ENCODER_PIN_A2 = 19;
-// - Encoder B
 const int ENCODER_PIN_B1 = 20, ENCODER_PIN_B2 = 21;
+
 // - Line Sensor A (front)
-const int LINE_SENSOR_PIN_A1 = A2, LINE_SENSOR_PIN_A2 = A1,
-                    LINE_SENSOR_PIN_A3 = A0;
-const int LINE_SENSOR_A_THRESHOLD = 350;
+const int LINE_SENSOR_PIN_A1 = A2;
+const int LINE_SENSOR_PIN_A2 = A1;
+const int LINE_SENSOR_PIN_A3 = A0;
+
 // - Line Sensor B (side)
-const int LINE_SENSOR_PIN_B1 = A3, LINE_SENSOR_PIN_B2 = A4,
-                    LINE_SENSOR_PIN_B3 = A5;
-const int LINE_SENSOR_B_THRESHOLD = 500;
+const int LINE_SENSOR_PIN_B1 = A3;
+const int LINE_SENSOR_PIN_B2 = A4;
+const int LINE_SENSOR_PIN_B3 = A5;
+
 // - Ultrasonic Sensor
-const int ULTRASONIC_TRIG_PIN = 5, ULTRASONIC_ECHO_PIN = 46; // TODO: Define pins
-// ===== OUTPUTS =====
+const int ULTRASONIC_TRIG_PIN = 5, ULTRASONIC_ECHO_PIN = 46;
+
+// ==================================
+// ===== OUTPUT PIN ASSIGNMENTS =====
+// ==================================
+
 // - LED
 const int LED_PIN = LED_BUILTIN;
+
 // - Servo Motor A (right)
-const int SERVO_PIN_A1 = 11, SERVO_PIN_A2 = 12; // TODO: Define pins
+const int SERVO_PIN_A1 = 11, SERVO_PIN_A2 = 12;
+
 // - Servo Motor B (left)
-const int SERVO_PIN_B1 = 10, SERVO_PIN_B2 = 9; // TODO: Define pins
+const int SERVO_PIN_B1 = 10, SERVO_PIN_B2 = 9;
+
 // - Stepper Motor A (four-bar)
-const int STEPPER_PIN_A1 = 40, STEPPER_PIN_A2 = 41, STEPPER_PIN_A3 = 42,
-                    STEPPER_PIN_A4 = 43;                  // TODO: Define pins
-const int STEPPER_A_STEPS_PER_REVOLUTION = 200; // TODO: Set this value
-const int STEPPER_A_MAX_SPEED = 30;             // TODO: Set this value
+const int STEPPER_PIN_A1 = 40, STEPPER_PIN_A2 = 41;
+const int STEPPER_PIN_A3 = 42, STEPPER_PIN_A4 = 43;
+
 // - Stepper Motor B (lift)
-const int STEPPER_PIN_B1 = 30, STEPPER_PIN_B2 = 31, STEPPER_PIN_B3 = 32,
-                    STEPPER_PIN_B4 = 33;                  // TODO: Define pins
-const int STEPPER_B_STEPS_PER_REVOLUTION = 200; // TODO: Set this value
-const int STEPPER_B_MAX_SPEED = 9;              // TODO: Set this value
-// ===== CONTROL GAINS =====
+const int STEPPER_PIN_B1 = 30, STEPPER_PIN_B2 = 31;
+const int STEPPER_PIN_B3 = 32, STEPPER_PIN_B4 = 33;
+
+// ===============================
+// ===== HARDWARE PARAMETERS =====
+// ===============================
+
+// - Stepper Motor A (four-bar)
+const int STEPPER_A_STEPS_PER_REVOLUTION = 200;
+
+// - Stepper Motor B (lift)
+const int STEPPER_B_STEPS_PER_REVOLUTION = 200;
+
+// ===== SOFTWARE SETTINGS =====
+
+// - Line Sensor A (front)
+const int LINE_SENSOR_A_THRESHOLD = 350;
+
+// - Line Sensor B (side)
+const int LINE_SENSOR_B_THRESHOLD = 500;
+
+// - Stepper Motor A (four-bar)
+const int STEPPER_A_MAX_SPEED = 30;
+
+// - Stepper Motor B (lift)
+const int STEPPER_B_MAX_SPEED = 9;
+
+// - Encoder PID drive parameters
 const float ENCODER_DRIVE_KP = 0.05;
 const float ENCODER_DRIVE_KD = 0.005;
 const float ENCODER_DRIVE_KI = 0.01;
 
+// - Line follow PID parameters
 const float LINE_FOLLOW_REGULAR_KP = 190.0;
 const float LINE_FOLLOW_REGULAR_KD = 0.0;
 const float LINE_FOLLOW_REGULAR_KI = 0.0;
+
+// ==================================
+// ==================================
+// ==================================
+
+#endif // CONFIG_H
+
+
 // All IO list:
 // INP button [1 pin]
 // INP encoder A (right wheel) [2 pins]
@@ -70,5 +121,3 @@ const float LINE_FOLLOW_REGULAR_KI = 0.0;
 // OUT servo B (left) [2 pins]
 // OUT stepper A (four-bar) [4 pins]
 // OUT stepper B (lift) [4 pins]
-
-#endif // CONFIG_H
